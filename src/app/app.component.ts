ad6fa07ee34c688c7563352d9036d202a5ec13b7
Mohammed Zaky <m.zaki.elkassed@gmail.com>
2023-03-07 22:32:02 +0400

    added type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+interface Book {
+  name: string;
+  id: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-exam';
+  //books = [{ id: 1, name: 'First book' }];
+  books: Book[] = [];
+
+  bookForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl('', Validators.required),
+  });
+  get bookName() {
+    return this.bookForm.get('name')
+  }
+  addBook() {
+    if (this.bookForm.valid) {
+      const title = this.bookForm.value.name!;
+      const book = { id: Math.random(), name: title };
+      this.books.push(book);
+      this.bookForm.reset();
+    }
+  }
+
+  deleteBook(bookId: number) {
+    const index = this.books.findIndex(book => book.id === bookId);
+    if (index >= 0) {
+      this.books.splice(index, 1);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [CommonModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
